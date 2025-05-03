415dcf9e8ea5c5322e9b49badec53328afc2b256
NkwiCyril <akinimbomnkwi@gmail.com>
2025-05-03 12:43:13 +0100

    why-choose-us section

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -71,62 +71,7 @@
     </section>
     <!-- BANNER SECTION END -->
 
-
-    <!-- WHY CHOOSE US SECTION START -->
-    <section class="ul-why-choose-us ul-section-spacing wow animate__fadeInUp">
-        <div class="ul-container">
-            <div class="row row-cols-lg-2 row-cols-1 align-items-center">
-                <div class="col">
-                    <div class="ul-why-choose-us-imgs">
-                        <div class="img"><img src="assets/img/why-choose-img-1.jpg" alt="image"></div>
-                        <div class="img">
-                            <img src="assets/img/why-choose-img-2.jpg" alt="image">
-                            <div class="icon"><i class="flaticon-home-agreement"></i></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="ul-why-choose-us-txt">
-                        <span class="ul-section-sub-title">Why Choose Brillian & Co Real Estate</span>
-                        <h2 class="ul-section-title">Your Trust is Our Commitment</h2>
-                        <p class="ul-why-choose-us-heading-descr">We deliver top-tier real estate services, tailored to
-                            your needs.</p>
-
-                        <div class="ul-why-choose-us-list">
-                            <div class="ul-why-choose-us-list-item">
-                                <div class="icon"><i class="flaticon-property"></i></div>
-                                <div class="txt">
-                                    <h3 class="ul-why-choose-us-list-item-title">Rentals & Leasing</h3>
-                                    <p class="ul-why-choose-us-list-item-descr">Studios, rooms, apartments, offices,
-                                        stores, and guesthouses available at competitive rates.</p>
-                                </div>
-                            </div>
-
-                            <div class="ul-why-choose-us-list-item">
-                                <div class="icon"><i class="flaticon-list-1"></i></div>
-                                <div class="txt">
-                                    <h3 class="ul-why-choose-us-list-item-title">Property Management</h3>
-                                    <p class="ul-why-choose-us-list-item-descr">Expert property care, maintenance, and
-                                        leasing for landlords and investors.</p>
-                                </div>
-                            </div>
-
-                            <div class="ul-why-choose-us-list-item">
-                                <div class="icon"><i class="flaticon-consulting"></i></div>
-                                <div class="txt">
-                                    <h3 class="ul-why-choose-us-list-item-title">Real Estate Consulting</h3>
-                                    <p class="ul-why-choose-us-list-item-descr">We provide guidance on investments,
-                                        development, and business marketing.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- WHY CHOOSE US SECTION END -->
-
+    @include('sections.why-choose-us')
     @include('sections.featured-properties')
     @include('sections.facilities')
     @include('sections.properties')
