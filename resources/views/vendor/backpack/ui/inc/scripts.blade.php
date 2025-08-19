ffe2cbaee6ec8591c33f622021657debcb314fdc
Nkwi Cyril <akinimbomnkwi@gmail.com>
2025-08-19 19:07:29 +0100

    Replaced @basset helpers with html script tag

--- resources/views/vendor/backpack/ui/inc/scripts.blade.php
+++ resources/views/vendor/backpack/ui/inc/scripts.blade.php
@@ -1,8 +1,7 @@
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/noty.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert@2.1.2/dist/sweetalert.min.js"></script>
-
+@basset('https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js')
+@basset('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js')
+@basset('https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/noty.min.js')
+@basset('https://cdn.jsdelivr.net/npm/sweetalert@2.1.2/dist/sweetalert.min.js')
 
 @if (backpack_theme_config('scripts') && count(backpack_theme_config('scripts')))
 @foreach (backpack_theme_config('scripts') as $path)
