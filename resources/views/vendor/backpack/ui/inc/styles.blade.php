7291f88178525aaae1152f37374c1de929222d6e
Nkwi Cyril <akinimbomnkwi@gmail.com>
2025-08-23 21:31:16 +0100

    Comment out animate.css basset import and adjust line-awesome CSS imports

--- resources/views/vendor/backpack/ui/inc/styles.blade.php
+++ resources/views/vendor/backpack/ui/inc/styles.blade.php
@@ -1,7 +1,7 @@
-@basset('https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.compat.css')
+{{-- @basset('https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.compat.css')
 @basset('https://cdn.jsdelivr.net/npm/noty@3.2.0-beta-deprecated/lib/noty.css')
+
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css')
-{{-- 
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-regular-400.woff2')
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-solid-900.woff2')
 @basset('https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/fonts/la-brands-400.woff2')
@@ -21,7 +21,7 @@
 
 @basset(base_path('vendor/backpack/crud/src/resources/assets/css/common.css'))
 
-@if (backpack_theme_config('styles') && count(backpack_theme_config('styles')))
+{{-- @if (backpack_theme_config('styles') && count(backpack_theme_config('styles')))
     @foreach (backpack_theme_config('styles') as $path)
         @if(is_array($path))
             @basset(...$path)
@@ -39,4 +39,4 @@
 
 @if (backpack_theme_config('vite_styles') && count(backpack_theme_config('vite_styles')))
     @vite(backpack_theme_config('vite_styles'))
-@endif
+@endif --}}
